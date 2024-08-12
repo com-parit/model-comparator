f72b80b4fc24b2a550a3fc5a4ae36e43a6cb5629
jawad571 <jawadmustafa571@gmail.com>
2024-08-12 01:50:40 +0100

    update frontend

--- usage/dashboard.py
+++ usage/dashboard.py
@@ -3,170 +3,289 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import json
-
-# Sample data
-df = pd.read_json("output/model_level_json.json")
+from adapter import Adapter
 
 st.title('Dashboard')
 
-try:
-	bar_df = pd.DataFrame({'x': [
-     "precision", 
-     "recall", 
-     "f1_score", 
-     "cosine_similarity_tfidf", 
-     "cosine_similarity_word2vec",
-    #  "ragas_answer_similarity"
-	], 'y': [
-		df.T["aggregate_model_precision"][0], 
-		df.T["aggregate_model_recall"][0], 
-		df.T["aggregate_model_f1_score"][0],
-		df.T["cosine_similarity_tfidf"][0],
-		df.T["cosine_similarity_word2vec"][0],
-		# df.T["ragas_answer_similarity"][0] if df.T["ragas_answer_similarity"][0] > 0 else 0 
-  	]
-	})
-	fig = px.bar(bar_df, x='x', y='y', title='Model Level Metrics')
-	st.plotly_chart(fig)
-except Exception as e:
-    print(e)
+model_type = st.selectbox("Choose Model Type", ["None", "Ecore", "Emf"])
 
-colors = ['green', 'grey', 'red']
-
-try:
-	pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
-		df.T["classes_tp"][0],
-		df.T["classes_fp"][0],
-		df.T["classes_fn"][0]
-	]})
-	pie = px.pie(pie_df, values='values', names='element', title='Classes')
-	pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
-	st.plotly_chart(pie)
-except Exception as e:
-    print(e)
-
-try:
-	pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
-		df.T["attributes_tp"][0],
-		df.T["attributes_fp"][0],
-		df.T["attributes_fn"][0]
-	]})
-	pie = px.pie(pie_df, values='values', names='element', title='Attributes')
-	pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
-	st.plotly_chart(pie)
-except Exception as e:
-    print(e)
-
-try:
-	pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
-		df.T["operations_tp"][0],
-		df.T["operations_fp"][0],
-		df.T["operations_fn"][0]
-	]})
-	pie = px.pie(pie_df, values='values', names='element', title='Operations')
-	pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
-	st.plotly_chart(pie)
-except Exception as e:
-    print(e)
-
-
-try:
-	pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
-		df.T["references_tp"][0],
-		df.T["references_fp"][0],
-		df.T["references_fn"][0]
-	]})
-	pie = px.pie(pie_df, values='values', names='element', title='References')
-	pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
-	st.plotly_chart(pie)
-except Exception as e:
-    print(e)
-
-try:
-	pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
-		df.T["superTypes_tp"][0],
-		df.T["superTypes_fp"][0],
-		df.T["superTypes_fn"][0]
-	]})
-	pie = px.pie(pie_df, values='values', names='element', title=f'Supertypes')
-	pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
-	st.plotly_chart(pie)
-except Exception as e:
-    print(e)
-
-with open("output/class_level_json.json", "r") as cj:
-    class_level_json = json.loads(cj.read())
-    columns = [
-        "name1", 
-        "name2", 
-        "attributes_tp",
-        "attributes_fp",
-        "attributes_fn",
-        "references_tp",
-        "references_fp",
-        "references_fn",
-        "superTypes_tp",
-        "superTypes_fp",
-        "superTypes_fn",
-        "operations_tp",
-        "operations_fp",
-        "operations_fn",
-    ]
-    values = []
-    for i in class_level_json:
-        names1 = []
-        names2 = []
-        attributes_tp = []
-        attributes_fp = []
-        attributes_fn = []
-        references_tp = []
-        references_fp = []
-        references_fn = []
-        superTypes_tp = []
-        superTypes_fp = []
-        superTypes_fn = []
-        operations_tp = []
-        operations_fp = []
-        operations_fn = []
-        for class_obj in class_level_json[i]:
-            names1.append(class_level_json[i][class_obj]["class_name_model1"])
-            names2.append(class_level_json[i][class_obj]["class_name_model2"])
-            attributes_tp.append(class_level_json[i][class_obj]["attributes_tp"])
-            attributes_fp.append(class_level_json[i][class_obj]["attributes_fp"])
-            attributes_fn.append(class_level_json[i][class_obj]["attributes_fn"])
-            references_tp.append(class_level_json[i][class_obj]["references_tp"])
-            references_fp.append(class_level_json[i][class_obj]["references_fp"])
-            references_fn.append(class_level_json[i][class_obj]["references_fn"])
-            operations_tp.append(class_level_json[i][class_obj]["operations_tp"])
-            operations_fp.append(class_level_json[i][class_obj]["operations_fp"])
-            operations_fn.append(class_level_json[i][class_obj]["operations_fn"])
-            superTypes_tp.append(class_level_json[i][class_obj]["superTypes_tp"])
-            superTypes_fp.append(class_level_json[i][class_obj]["superTypes_fp"])
-            superTypes_fn.append(class_level_json[i][class_obj]["superTypes_fn"])
-        values.append(names1)
-        values.append(names2)
-        values.append(attributes_tp)
-        values.append(attributes_fp)
-        values.append(attributes_fn)
-        values.append(references_tp)
-        values.append(references_fp)
-        values.append(references_fn)
-        values.append(superTypes_tp)
-        values.append(superTypes_fp)
-        values.append(superTypes_fn)
-        values.append(operations_tp)
-        values.append(operations_fp)
-        values.append(operations_fn)
+col1, col2 = st.columns(2)
+if model_type == "Ecore":
+    with col1:
+        ground_truth_model_ecore = st.text_area(
+            "Input Ecore Ground Truth Model",
+        )
+    with col2:
+        predicted_model_ecore = st.text_area(
+            "Input Ecore Predicted Model"
+        )
+elif model_type == "Emf":
+    with col1:
+        ground_truth_model_ecore = st.text_area(
+            "Input Ecore EMF Truth Model",
+        )
+    with col2:
+        predicted_model_ecore = st.text_area(
+            "Input EMF Predicted Model"
+        )
     
-    fig = go.Figure(data=[go.Table(
-        header=dict(values=columns,
-			line_color='darkslategray',
-			fill_color='maroon',
-			align='left'
-		),
-        cells=dict(values=values,
-				line_color='darkslategray',
-				fill_color='black',
-				align='left'))])
-    fig.update_layout(width=600, height=1000)
-    st.plotly_chart(fig)
+
+project_name = st.text_input("Input Name of Project")
+st.write("Configuration Panel")
+with st.container(height=400, border=True):    
+    config_col1, config_col2, config_col3, config_col4, config_col5 = st.columns(5)
+
+    with config_col1:
+        USE_HASHING = st.selectbox("Use Hashing", ["True", "False"]) == "True"
+        HASHING_THRESHOLD = st.text_input("Hashing Threshold", value="0.95")
+
+    with config_col2:
+        INCLUDE_CLASS_ATTRIBUTES = st.selectbox("Include Class Attributes", ["True", "False"]) == "True"
+        INCLUDE_CLASS_REFERENCES = st.selectbox("Include Class References", ["True", "False"]) == "True"
+        INCLUDE_CLASS_OPERATIONS = st.selectbox("Include Class Operations", ["True", "False"]) == "True"
+        INCLUDE_CLASS_SUPERTYPES = st.selectbox("Include Class Supertypes", ["True", "False"]) == "True"
+        INCLUDE_ENUMS = st.selectbox("Include Enums", ["True", "False"]) == "True"
+
+    with config_col3:
+        INCLUDE_ATTRIBUTE_NAME = st.selectbox("Include Attribute Name", ["True", "False"]) == "True"
+        INCLUDE_ATTRIBUTE_CONTAINING_CLASS = st.selectbox("Include Attribute Containing Class", ["True", "False"]) == "True"
+        INCLUDE_ATTRIBUTE_TYPE = st.selectbox("Include Attribute Type", ["True", "False"]) == "True"
+        INCLUDE_ATTRIBUTE_LOWER_BOUND = st.selectbox("Include Attribute Lower Bound", ["True", "False"]) == "True"
+        INCLUDE_ATTRIBUTE_UPPER_BOUND = st.selectbox("Include Attribute Upper Bound", ["True", "False"]) == "True"
+        INCLUDE_ATTRIBUTE_IS_ORDERED = st.selectbox("Include Attribute Is Ordered", ["True", "False"]) == "True"
+        INCLUDE_ATTRIBUTE_IS_UNIQUE = st.selectbox("Include Attribute Is Unique", ["True", "False"]) == "True"
+
+    with config_col4:
+        INCLUDE_REFERENCES_NAME = st.selectbox("Include References Name", ["True", "False"]) == "True"
+        INCLUDE_REFERENCES_CONTAINING_CLASS = st.selectbox("Include References Containing Class", ["True", "False"]) == "True"
+        INCLUDE_REFERENCES_IS_CONTAINMENT = st.selectbox("Include References Is Containment", ["True", "False"]) == "True"
+        INCLUDE_REFERENCES_LOWER_BOUND = st.selectbox("Include References Lower Bound", ["True", "False"]) == "True"
+        INCLUDE_REFERENCES_UPPER_BOUND = st.selectbox("Include References Upper Bound", ["True", "False"]) == "True"
+        INCLUDE_REFERENCES_IS_ORDERED = st.selectbox("Include References Is Ordered", ["True", "False"]) == "True"
+        INCLUDE_REFERENCES_IS_UNIQUE = st.selectbox("Include References Is Unique", ["True", "False"]) == "True"
+
+    with config_col5:
+        INCLUDE_OPERATION_NAME = st.selectbox("Include Operation Name", ["True", "False"]) == "True"
+        INCLUDE_OPERATION_CONTAINING_CLASS = st.selectbox("Include Operation Containing Class", ["True", "False"]) == "True"
+        INCLUDE_OPERATION_PARAMETERS = st.selectbox("Include Operation Parameters", ["True", "False"]) == "True"
+        INCLUDE_PARAMETER_NAME = st.selectbox("Include Parameter Name", ["True", "False"]) == "True"
+        INCLUDE_PARAMETER_TYPE = st.selectbox("Include Parameter Type", ["True", "False"]) == "True"
+        INCLUDE_PARAMETER_OPERATION_NAME = st.selectbox("Include Parameter Operation Name", ["True", "False"]) == "True"
+    
+if st.button("Compare", type="primary"):
+    config = {
+        "USE_HASHING": USE_HASHING,
+        "HASHING_THRESHOLD": float(HASHING_THRESHOLD),
+        "INCLUDE_CLASS_ATTRIBUTES": INCLUDE_CLASS_ATTRIBUTES,
+        "INCLUDE_CLASS_REFERENCES": INCLUDE_CLASS_REFERENCES,
+        "INCLUDE_CLASS_OPERATIONS": INCLUDE_CLASS_OPERATIONS,
+        "INCLUDE_CLASS_SUPERTYPES": INCLUDE_CLASS_SUPERTYPES,
+        "INCLUDE_ENUMS": INCLUDE_ENUMS,
+        "INCLUDE_ATTRIBUTE_NAME": INCLUDE_ATTRIBUTE_NAME,
+        "INCLUDE_ATTRIBUTE_CONTAINING_CLASS": INCLUDE_ATTRIBUTE_CONTAINING_CLASS,
+        "INCLUDE_ATTRIBUTE_TYPE": INCLUDE_ATTRIBUTE_TYPE,
+        "INCLUDE_ATTRIBUTE_LOWER_BOUND": INCLUDE_ATTRIBUTE_LOWER_BOUND,
+        "INCLUDE_ATTRIBUTE_UPPER_BOUND": INCLUDE_ATTRIBUTE_UPPER_BOUND,
+        "INCLUDE_ATTRIBUTE_IS_ORDERED": INCLUDE_ATTRIBUTE_IS_ORDERED,
+        "INCLUDE_ATTRIBUTE_IS_UNIQUE": INCLUDE_ATTRIBUTE_IS_UNIQUE,
+        "INCLUDE_REFERENCES_NAME": INCLUDE_REFERENCES_NAME,
+        "INCLUDE_REFERENCES_CONTAINING_CLASS": INCLUDE_REFERENCES_CONTAINING_CLASS,
+        "INCLUDE_REFERENCES_IS_CONTAINMENT": INCLUDE_REFERENCES_IS_CONTAINMENT,
+        "INCLUDE_REFERENCES_LOWER_BOUND": INCLUDE_REFERENCES_LOWER_BOUND,
+        "INCLUDE_REFERENCES_UPPER_BOUND": INCLUDE_REFERENCES_UPPER_BOUND,
+        "INCLUDE_REFERENCES_IS_ORDERED": INCLUDE_REFERENCES_IS_ORDERED,
+        "INCLUDE_REFERENCES_IS_UNIQUE": INCLUDE_REFERENCES_IS_UNIQUE,
+        "INCLUDE_OPERATION_NAME": INCLUDE_OPERATION_NAME,
+        "INCLUDE_OPERATION_CONTAINING_CLASS": INCLUDE_OPERATION_CONTAINING_CLASS,
+        "INCLUDE_OPERATION_PARAMETERS": INCLUDE_OPERATION_PARAMETERS,
+        "INCLUDE_PARAMETER_NAME": INCLUDE_PARAMETER_NAME,
+        "INCLUDE_PARAMETER_TYPE": INCLUDE_PARAMETER_TYPE,
+        "INCLUDE_PARAMETER_OPERATION_NAME": INCLUDE_PARAMETER_OPERATION_NAME
+    }
+
+    # Save to JSON file
+    with open("config_user.json", "w") as outfile:
+        json.dump(config, outfile, indent=4)
+    st.write("Comparing...")
+    config_file_path = "config_user.json"
+    ground_truth_path = "ground_truth_model.ecore" if model_type == "Ecore" else "ground_truth_model.emf"
+    predicted_path = "predicted_model.ecore" if model_type == "Ecore" else "predicted_model.emf"
+    with open(config_file_path, 'w') as fr:
+        fr.write(str(config))  
+    with open(ground_truth_path, 'w') as fr:
+        fr.write(str(ground_truth_model_ecore))  
+    with open(predicted_path, 'w') as fr:
+        fr.write(str(predicted_model_ecore))  
+        
+    if model_type == "Ecore":
+        model_level_json, class_level_json = Adapter.compare_ecore_models_syntactically_and_semantically(ground_truth_path, predicted_path, project_name, config_file_path)
+    elif model_type == "Emf":
+        model_level_json, class_level_json = Adapter.compare_emfatic_models_syntactically_and_semantically(ground_truth_path, predicted_path, project_name, config_file_path)
+    
+    with open("model_level_json.json", 'w') as fr:
+        fr.write(json.dumps(model_level_json, indent=4)) 
+    with open("class_level_json.json", 'w') as fr:
+        fr.write(json.dumps(class_level_json, indent=4)) 
+
+    df = pd.read_json("model_level_json.json")
+    try:
+        bar_df = pd.DataFrame({'x': [
+        "precision", 
+        "recall", 
+        "f1_score", 
+        # "cosine_similarity_tfidf", 
+        # "cosine_similarity_word2vec",
+        #  "ragas_answer_similarity"
+        ], 'y': [
+            df.T["aggregate_model_precision"][0], 
+            df.T["aggregate_model_recall"][0], 
+            df.T["aggregate_model_f1_score"][0],
+            # df.T["cosine_similarity_tfidf"][0],
+            # df.T["cosine_similarity_word2vec"][0],
+            # df.T["ragas_answer_similarity"][0] if df.T["ragas_answer_similarity"][0] > 0 else 0 
+        ]
+        })
+        fig = px.bar(bar_df, x='x', y='y', title='Model Level Metrics')
+        st.plotly_chart(fig)
+    except Exception as e:
+        print(e)
+
+    colors = ['green', 'grey', 'red']
+
+    try:
+        pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
+            df.T["classes_tp"][0],
+            df.T["classes_fp"][0],
+            df.T["classes_fn"][0]
+        ]})
+        pie = px.pie(pie_df, values='values', names='element', title='Classes')
+        pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
+        st.plotly_chart(pie)
+    except Exception as e:
+        print(e)
+
+    try:
+        pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
+            df.T["attributes_tp"][0],
+            df.T["attributes_fp"][0],
+            df.T["attributes_fn"][0]
+        ]})
+        pie = px.pie(pie_df, values='values', names='element', title='Attributes')
+        pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
+        st.plotly_chart(pie)
+    except Exception as e:
+        print(e)
+
+    try:
+        pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
+            df.T["operations_tp"][0],
+            df.T["operations_fp"][0],
+            df.T["operations_fn"][0]
+        ]})
+        pie = px.pie(pie_df, values='values', names='element', title='Operations')
+        pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
+        st.plotly_chart(pie)
+    except Exception as e:
+        print(e)
+
+
+    try:
+        pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
+            df.T["references_tp"][0],
+            df.T["references_fp"][0],
+            df.T["references_fn"][0]
+        ]})
+        pie = px.pie(pie_df, values='values', names='element', title='References')
+        pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
+        st.plotly_chart(pie)
+    except Exception as e:
+        print(e)
+
+    try:
+        pie_df = pd.DataFrame({'element': ["Correctly Identified", "Only in Model 2", "Only in Model 1"], 'values': [
+            df.T["superTypes_tp"][0],
+            df.T["superTypes_fp"][0],
+            df.T["superTypes_fn"][0]
+        ]})
+        pie = px.pie(pie_df, values='values', names='element', title=f'Supertypes')
+        pie.update_traces(textposition='inside', textinfo='value', marker=dict(colors=colors))
+        st.plotly_chart(pie)
+    except Exception as e:
+        print(e)
+
+    with open("class_level_json.json", "r") as cj:
+        class_level_json = json.loads(cj.read())
+        columns = [
+            "name1", 
+            "name2", 
+            "attributes_tp",
+            "attributes_fp",
+            "attributes_fn",
+            "references_tp",
+            "references_fp",
+            "references_fn",
+            "superTypes_tp",
+            "superTypes_fp",
+            "superTypes_fn",
+            "operations_tp",
+            "operations_fp",
+            "operations_fn",
+        ]
+        values = []
+        for i in class_level_json:
+            names1 = []
+            names2 = []
+            attributes_tp = []
+            attributes_fp = []
+            attributes_fn = []
+            references_tp = []
+            references_fp = []
+            references_fn = []
+            superTypes_tp = []
+            superTypes_fp = []
+            superTypes_fn = []
+            operations_tp = []
+            operations_fp = []
+            operations_fn = []
+            for class_obj in class_level_json[i]:
+                names1.append(class_level_json[i][class_obj]["class_name_model1"])
+                names2.append(class_level_json[i][class_obj]["class_name_model2"])
+                attributes_tp.append(class_level_json[i][class_obj]["attributes_tp"])
+                attributes_fp.append(class_level_json[i][class_obj]["attributes_fp"])
+                attributes_fn.append(class_level_json[i][class_obj]["attributes_fn"])
+                references_tp.append(class_level_json[i][class_obj]["references_tp"])
+                references_fp.append(class_level_json[i][class_obj]["references_fp"])
+                references_fn.append(class_level_json[i][class_obj]["references_fn"])
+                operations_tp.append(class_level_json[i][class_obj]["operations_tp"])
+                operations_fp.append(class_level_json[i][class_obj]["operations_fp"])
+                operations_fn.append(class_level_json[i][class_obj]["operations_fn"])
+                superTypes_tp.append(class_level_json[i][class_obj]["superTypes_tp"])
+                superTypes_fp.append(class_level_json[i][class_obj]["superTypes_fp"])
+                superTypes_fn.append(class_level_json[i][class_obj]["superTypes_fn"])
+            values.append(names1)
+            values.append(names2)
+            values.append(attributes_tp)
+            values.append(attributes_fp)
+            values.append(attributes_fn)
+            values.append(references_tp)
+            values.append(references_fp)
+            values.append(references_fn)
+            values.append(superTypes_tp)
+            values.append(superTypes_fp)
+            values.append(superTypes_fn)
+            values.append(operations_tp)
+            values.append(operations_fp)
+            values.append(operations_fn)
+        
+        fig = go.Figure(data=[go.Table(
+            header=dict(values=columns,
+                line_color='darkslategray',
+                fill_color='maroon',
+                align='left'
+            ),
+            cells=dict(values=values,
+                    line_color='darkslategray',
+                    fill_color='black',
+                    align='left'))])
+        fig.update_layout(width=600, height=1000)
+        st.plotly_chart(fig)
